a18496c56fa8b3aac70af1dac4d6977c6143ff77
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-07-08 12:36:08 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from flask import Flask
 import alpaca_trade_api as tradeapi 
 
 alpaca_endpoint = 'https://paper-api.alpaca.markets'
-api = tradeapi.REST('JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
+api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
 
 #app = Flask(__name__)
 #@app.route("/")#URL leading to method
