581012eb5cfddabcbe3d664f7db7c0d398a35b5f
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-07-08 12:23:45 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
 from flask import Flask
+import alpaca_trade_api as tradeapi 
+
 app = Flask(__name__)
 @app.route("/")#URL leading to method
-def hello(): # Name of the method
- return("Hello World!") 
+
+account = api.getaccount()
+Print(account.status)
+
+alpaca_endpoint = 'https://paper-api.alpaca.markets'
+api = tradeapi.REST('JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
+
+
+
 if __name__ == "__main__":
  app.run(host='0.0.0.0', port='8080') 
