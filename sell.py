0e5d93d65b3f0736ac99a6d2859ea0640ba73f35
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-07-09 12:30:00 +0100

    Create sell.py

---
+++ sell.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import alpaca_trade_api as tradeapi 
+import schedule, time
+
+alpaca_endpoint = 'https://paper-api.alpaca.markets'
+api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
+
+#account = api.get_account()
+#print(account)
+
+
+#print(order)
+
+def sell_aapl(): 
+  print('Selling Apple')
+  order = api.submit_order(symbol='AAPL', qty=1, side='sell')
+  print(order)
+  
+schedule.every(1).hours.do(sell_aapl)
+
+while True:
+  schedule.run_pending()
+  time.sleep(1)
