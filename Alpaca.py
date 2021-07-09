24a67b0bb55f6bf5ed64f7938b8b3679898b88dd
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-07-09 11:28:54 +0100

    Update Alpaca.py

--- Alpaca.py
+++ Alpaca.py
@@ -7,3 +7,8 @@ api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPF
 account = api.get_account()
 print(account)
 
+
+api.submit_order(symbol='AAPL', qty=1, side='buy')
+
+print(order)
+
