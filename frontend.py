1b660d76be13b884c6b922c5085a5f8d26adb1c1
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-07-09 11:39:54 +0100

    Create frontend.py

---
+++ frontend.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import alpaca_trade_api as tradeapi 
+
+app = Flask(__name__)
+@app.route("/")#URL leading to method
+
+alpaca_endpoint = 'https://paper-api.alpaca.markets'
+api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
+
+account = api.get_account()
+print(account)
+
+
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port='8080') 
