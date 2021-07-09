58b7646ff83cc4fbeb70a17e940955c0e73c37f5
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-07-09 11:41:45 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -4,8 +4,8 @@ import alpaca_trade_api as tradeapi
 alpaca_endpoint = 'https://paper-api.alpaca.markets'
 api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
 
-app = Flask(__name__)
-@app.route("/")#URL leading to method
+#app = Flask(__name__)
+#@app.route("/")#URL leading to method
 
 account = api.get_account()
 print(account)
