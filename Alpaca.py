3dbbaca7c111e1f41107cfaacffcaf853f34e5a8
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-07-09 11:09:04 +0100

    Update Alpaca.py

--- Alpaca.py
+++ Alpaca.py
@@ -5,5 +5,5 @@ alpaca_endpoint = 'https://paper-api.alpaca.markets'
 api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
 
 account = api.get_account()
-Print(account)
+print(account)
 
