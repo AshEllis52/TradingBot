c09acbc6ade0dc4e63967aad6f8b629634e76c9f
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-13 12:00:34 +0100

    Update sma.py

--- sma.py
+++ sma.py
@@ -13,45 +13,32 @@ API_Secret = 'JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN'
 API_End = 'https://paper-api.alpaca.markets'
 
 
-class LongShort:
-  def __init__(self): #self, thats just the way it is. Doesn't have to be self can be anything. 
+class EmaSma:
+  def __init__(self):
     self.alpaca = tradeapi.REST(API_Key, API_Secret, API_End, 'v2')
 
-    stockUniverse = ['AAPL', 'BA', 'SPY', 'TSLA' ]
-    # Format the allStocks variable for use in the class.
-    self.allStocks = []
-    for stock in stockUniverse: #python for loop, doesnt matter what is used for 'stock' on the left, loop will itierate through stockUniverse  
-      self.allStocks.append([stock, 0]) #.append adds an element to the end of a list, in this case it is adding all elements of stockUniverse to allStocks
-
-   
-   
-  def run(self): #run method used to run the entire class on last line 
-    
+  def run(self):  
    start = dt.datetime(2000, 1, 1)
    end = dt.datetime(2021,12,31)
    df = web.DataReader('TSLA', 'stooq', start, end)
-   df1 = df.to_csv('tsla.csv')
-   #df1 = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
    # Calculate the EMA
-   sma = df1.rolling(20).mean()
-   ema = df1.rolling(50).mean()
-   sma['Close'] = talib.SMA(df1['Close'], timeperiod = 20)
-   ema['Close'] = talib.EMA(df1['Close'], timeperiod = 50 )
-  
+   #sma = df.rolling(20).mean()
+   #ema = df.rolling(20).mean()
+   sma = talib.SMA(df['Close'], timeperiod = 20)
+   ema = talib.EMA(df['Close'], timeperiod = 20 )
+   
     
-  
+   
    if(sma > ema): 
-      print('SMA has crossed EMA')
-      order = api.submit_order(symbol='TSLA', qty=1, side='buy')
-      print(order)
-      
+    print('SMA has crossed EMA')
+    order = api.submit_order(symbol='TSLA', qty=1, side='buy')
+    print(order)
+   
+   else:
+      pass 
 
-ls = LongShort()
-ls.run()
-
-
-
-
+es = EmaSma()
+es.run()
 
 
 
@@ -63,10 +50,5 @@ ls.run()
 
 
 
-#df['SMA'] = talib.SMA(df['Close'],timeperiod = 50)
-   # Calculate the EMA
-   #df['EMA'] = talib.EMA(df['Close'],timeperiod = 50)
-   #signal[SMA > EMA] = 1
-   #signal[EMA < SMA ] = -1
-   # Define the strategy
-   #bt_strategy = bt.Strategy('EMA_crossover',[bt.algos.WeighTarget(signal), bt.algos.Rebalance()])
+
+
