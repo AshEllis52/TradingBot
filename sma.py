9ac17257d37479c88afffd32fd138838bb11b83a
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-13 14:20:49 +0100

    Update sma.py

--- sma.py
+++ sma.py
@@ -14,7 +14,7 @@ API_End = 'https://paper-api.alpaca.markets'
 signal = signal
 
 
-class EmaSma:
+class RSI:
   def __init__(self):
     self.alpaca = api.REST(API_Key, API_Secret, API_End, 'v2')
 
@@ -22,22 +22,34 @@ class EmaSma:
    start = dt.datetime(2000, 1, 1)
    end = dt.datetime(2021,12,31)
    df = web.DataReader('TSLA', 'stooq', start, end)
+   signal = 0
    df['RSI'] = talib.RSI(df['Close'])
-   signal[df['RSI'] > 70] = -1
-   signal[df['RSI'] < 30] = 1
-   signal[(df['RSI'] <= 70) & (df['RSI'] >= 30)] = 0
-
+    if (df['RSI'] > 70):
+      signal = 1
+    elif (df['RSI'] < 30):
+      signal = 2
+    elif (df['RSI'] <= 70) & (df['RSI'] >= 30)]:
+      signal = 3
    
    
-   if(signal == 0): 
-    print('SMA has crossed EMA')
-    order = api.submit_order(symbol='TSLA', qty=1, side='buy')
-    print(order)
+   if(signal == 1): 
+    print('1')
+    #order = api.submit_order(symbol='TSLA', qty=1, side='buy')
+    #rint(order)
+   elif(signal == 2)
+    print('2')
+   elif(signal == 3)
+    print('3')
+    
+    
+    
    
+   #signal[df['RSI'] > 70] = -1
+   #signal[df['RSI'] < 30] = 1
+   #signal[(df['RSI'] <= 70) & (df['RSI'] >= 30)] = 0
 
-
-es = EmaSma()
-es.run()
+RSI = rsi()
+rsi.run()
 
 
 
