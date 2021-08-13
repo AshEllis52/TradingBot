6306d36f9555e49bd05314acdd9b6b869cf11b88
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-13 14:40:57 +0100

    Update sma.py

--- sma.py
+++ sma.py
@@ -12,12 +12,12 @@ API_Key = 'PKZMPG3T5B4KNAFUTRF3'
 API_Secret = 'JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN'
 API_End = 'https://paper-api.alpaca.markets'
 
-self.signal = set()
+
 
 class RSI:
   def __init__(self):
     self.alpaca = api.REST(API_Key, API_Secret, API_End, 'v2')
-
+    self.signal = set()
   def run(self):  
    start = dt.datetime(2000, 1, 1)
    end = dt.datetime(2021,12,31)
