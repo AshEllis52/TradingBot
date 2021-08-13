80b02b15f8b3477f1e59adc52807c8658de7fc22
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-13 11:24:02 +0100

    Update sma.py

--- sma.py
+++ sma.py
@@ -29,22 +29,20 @@ class LongShort:
     
    start = dt.datetime(2000, 1, 1)
    end = dt.datetime(2021,12,31)
-   df = web.DataReader('TSLA', 'stooq', start, end)
+   df = abs(web.DataReader('TSLA', 'stooq', start, end)) 
    # Calculate the EMA
    sma = df.rolling(20).mean()
    ema = df.rolling(50).mean()
-   emaP = ema['Close'].pct_change() * 100
-   smaP = sma['Close'].pct_change() * 100
-   #sma['Close'] = talib.EMA(df['Close'], timeperiod = 20)
-   #ema['Close'] = talib.EMA(df['Close'], timeperiod = 50 )
+   sma['Close'] = talib.EMA(df['Close'], timeperiod = 20)
+   ema['Close'] = talib.EMA(df['Close'], timeperiod = 50 )
   
     
   
    return print (emaP, smaP) 
-   #if(smaP > emaP): 
-      #print('SMA has crossed EMA')
-      #order = api.submit_order(symbol='TSLA', qty=1, side='buy')
-      #print(order)
+   if(sma > ema): 
+      print('SMA has crossed EMA')
+      order = api.submit_order(symbol='TSLA', qty=1, side='buy')
+      print(order)
       
 
 ls = LongShort()
