3637172407443212ef613e804d99750c95500a73
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-14 09:10:14 +0100

    Update sma.py

--- sma.py
+++ sma.py
@@ -21,18 +21,19 @@ class RSI:
    start = dt.datetime(2000, 1, 1)
    end = dt.datetime(2021,12,31)
    df = web.DataReader('TSLA', 'stooq', start, end)
-   df_RSI = talib.RSI(df['Close'].head())
-   signal = df_RSI.copy()
-   signal[df_RSI.isnull()] = 0
+   df_RSI = talib.RSI(df['Close'])
+   signal = df_RSI.to_numpy()
+   #signal = df_RSI.copy()
+   #signal[df_RSI.isnull()] = 0
    #signal[df_RSI > 70] = 1
    #signal[df_RSI < 30] = 2
    #signal[(df_RSI <= 70) & (df_RSI >= 30)] = 3
   
    
-   if(pd.Series(signal[df_RSI] > 0).all()):
+   if(signal > 0):
     print ('1')
-   elif(pd.Series(df_RSI < 30).all()):
-    print ('2')
+   #elif(pd.Series(df_RSI < 30).all()):
+    #print ('2')
    #elif([(pd.Series(df_RSI <= 70)) & (pd.Series(df_RSI >= 30))].any()): 
     #print ('3')
        
