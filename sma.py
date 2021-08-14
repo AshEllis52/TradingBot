b9e9de89e78d136876b2dba0c872ec67f05e89eb
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-14 09:48:22 +0100

    Update sma.py

--- sma.py
+++ sma.py
@@ -22,7 +22,8 @@ class RSI:
    end = dt.datetime(2021,12,31)
    df = web.DataReader('TSLA', 'stooq', start, end)
    df_RSI = talib.RSI(df['Close'])
-   print(df_RSI)
+   df1 = df_RSI.to_numpy()
+   print(df1.vaules[-1])
    #signal = df_RSI.copy()
    #signal[df_RSI.isnull()] = 0
    #signal[df_RSI > 70] = 1
