7395fbd9b92680a7e5be2ea8756cc8666fd3a9f2
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-14 08:44:54 +0100

    Update sma.py

--- sma.py
+++ sma.py
@@ -27,8 +27,8 @@ class RSI:
     print ('1')
    elif(pd.Series(df_RSI < 30).all()):
     print ('2')
-   elif([(pd.Series(df_RSI <= 70)) & (pd.Series(df_RSI >= 30))].any()): 
-    print ('3')
+   #elif([(pd.Series(df_RSI <= 70)) & (pd.Series(df_RSI >= 30))].any()): 
+    #print ('3')
        
 rsi = RSI()
 rsi.run()
