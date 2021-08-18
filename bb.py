1f9367381c36b98ba9ac5a0eb854300cec0e5916
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-18 14:36:46 +0100

    Create bb.py

---
+++ bb.py
@@ -0,0 +1,30 @@
+import alpaca_trade_api as tradeapi
+import datetime as dt
+from datetime import date 
+import pandas as pd 
+import pandas_datareader.data as web
+import talib
+
+alpaca_endpoint = 'https://paper-api.alpaca.markets'
+api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
+
+
+class BB:
+  def run(self):  
+   start = dt.datetime(2021, 1, 1)
+   end = date.today()
+   df = web.DataReader('BA', 'stooq', start, end)
+   upper, mid, lower = talib.BBANDS(df['Close'], nbdevup=2, nbdevdn=2, timeperiod=20)
+   print(upper) 
+   print(mid) 
+   print(lower) 
+   
+    
+  
+  
+  
+  
+    
+    
+bb = BB()
+bb.run()
