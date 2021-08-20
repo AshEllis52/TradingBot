939fcc013bc59f55c703d58c6652d6220f27f780
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-08-20 14:22:16 +0100

    Update bb.py

--- bb.py
+++ bb.py
@@ -12,22 +12,24 @@ api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPF
 class BB:
   def run(self):  
    start = dt.datetime(2021, 1, 1)
-   end = date.today()
+   end = date.today(df['Close'])
    df = web.DataReader('BA', 'stooq', start, end)
-   upper, mid, lower = talib.BBANDS(df['Close'], nbdevup=2, nbdevdn=2, timeperiod=20)
+   upper, mid, lower = talib.BBANDS(df['Close'], nbdevup=2, nbdevdn=2, timeperiod = 20)
    upper.to_numpy()
    mid.to_numpy()
    lower.to_numpy()
-   print(upper.values[19]) 
-   print(mid.values[19]) 
-   print(lower.values[19]) 
-   
+  
+  
+   if((mid.values[19]) > (upper.values[24])): #if SMA(mid) today is above what upper was 5 days ago stock is overbought 
+    print("Stock is above mean value, selling position")
+    api.submit_order(symbol='BA', qty=10, side='sell')
     
-  
-  
-  
-  
+   elif (mid.values[19] < lower.values[24]):
+    print("Stock is below mean value, buying position") #if SMA(mid) today is below what lower was 5 days ago stock is underbought  
+    api.submit_order(symbol='BA', qty=10, side='buy')
     
-    
+   elif [((mid.values[19]) > lower.values[24]) & ((mid.values[19] < upper.values[24])]:
+    print("Stock is within mean value, holding position") #if SMA(mid) today is between  lower & upper 5 days ago stock is at mean value  
+       
 bb = BB()
 bb.run()
