dc011ba283a9a9642e08c1e257a4608d751563f5
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-09-02 12:31:32 +0100

    Update doji.py

--- doji.py
+++ doji.py
@@ -18,25 +18,26 @@ class DOJI:
   def run(self):  
    start = dt.datetime(2021, 1, 1)
    end = date.today()
-   df = web.get_data_yahoo('BB', start, end)
+   df = web.get_data_yahoo('DELL', start, end)
    df_d = talib.CDLDOJISTAR(df['Open'], df['High'], df['Low'], df['Close'])
    df_e = talib.CDLENGULFING(df['Open'], df['High'], df['Low'], df['Close'])
    df_d.to_numpy()
    df_e.to_numpy()
   
-  #if [(df_d.values[0] = 100) & (df_e.values[0] = 100)]:
-   #print('The patterns indicates a trough postion on the Candlestick Graph, buying postion') 
-   #api.submit_order(symbol='SPY', qty=100, side='buy')
+  if [(df_d.values[0] = 100) or (df_e.values[0] = 100)]:
+   print('The patterns indicates a trough postion on the Candlestick Graph, buying postion') 
+   api.submit_order(symbol='DELL', qty=100, side='buy')
     
-   #elif [((todayPrice.values[0]) < (upper.values[19])) & ((todayPrice.values[0]) > (lower.values[19]))]: 
-   #print("Stock is within mean value, holding position")  
+  elif [(df_d.values[0] = -100) or (df_e.values[0] = -100)]:
+   print('The patterns indicates a peak postion on the Candlestick Graph, selling postion') 
+   api.submit_order(symbol='DELL', qty=100, side='sell')  
     
+  elif [(df_d.values[0] = 0) or (df_e.values[0] = 0)]:
+   print('The patterns indicates holding postion')     
     
-    
-    
-   print(df_d[df_d !=0])
-   print(df_e[df_e !=0])
-   print(df_d.values[0])
+  print('Searching historical values that do not equal zero so that we can see days where the conditions of Doji are met') 
+  print(df_d[df_d !=0])
+  print(df_e[df_e !=0])
 
 doji = DOJI()
 doji.run()
