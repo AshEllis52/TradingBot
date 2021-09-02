1ae05402e6c2769172e3da2465bee5807fad666f
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-09-02 12:47:35 +0100

    Update doji.py

--- doji.py
+++ doji.py
@@ -24,13 +24,19 @@ class DOJI:
    df_d.to_numpy()
    df_e.to_numpy()
    
-    #if [(df_d.values[0] > 0) or (df_e.values[0] > 0)]:
    if (df_d.values[0] > 0):
     print('The patterns indicates a trough postion on the Candlestick Graph, buying postion') 
     api.submit_order(symbol='DELL', qty=100, side='buy')
     
-    #elif [(df_d.values[0] < 0) & (df_e.values[0] < 0)]:
+   elif (df_e.values[0] > 0):
+    print('The patterns indicates a trough postion on the Candlestick Graph, buying postion') 
+    api.submit_order(symbol='DELL', qty=100, side='buy') 
+   
    elif (df_d.values[0] < 0):
+    print('The patterns indicates a peak postion on the Candlestick Graph, selling postion') 
+    api.submit_order(symbol='DELL', qty=100, side='sell')  
+    
+   elif (df_e.values[0] < 0):
     print('The patterns indicates a peak postion on the Candlestick Graph, selling postion') 
     api.submit_order(symbol='DELL', qty=100, side='sell')  
     
@@ -40,8 +46,6 @@ class DOJI:
    print('Searching historical values that do not equal zero so that we can see days where the conditions of Doji are met') 
    print(df_d[df_d !=0])
    print(df_e[df_e !=0])
-   print(df_d.values[0])
-   print(df_e.values[0])
 
 doji = DOJI()
 doji.run()
