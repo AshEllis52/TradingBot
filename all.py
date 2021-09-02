4e433bcc5c4fda079e61cfa0546f6037deb32c94
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-09-02 12:55:43 +0100

    Update all.py

--- all.py
+++ all.py
@@ -4,6 +4,8 @@ from datetime import date
 import pandas as pd 
 import pandas_datareader.data as web
 import talib
+import yfinance as yfin
+yfin.pdr_override()
 
 alpaca_endpoint = 'https://paper-api.alpaca.markets'
 api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
@@ -13,7 +15,7 @@ class ADX:
   def run(self):  
    start = dt.datetime(2021, 1, 1)
    end = date.today()
-   df = web.DataReader('SPY', 'stooq', start, end)
+   df = web.get_data_yahoo('SPY', start, end)
    adx = talib.ADX(df['High'], df['Low'], df['Close'], timeperiod = 21)
    adx.to_numpy()
   
@@ -36,7 +38,7 @@ class EMA:
   def run(self):  
    start = dt.datetime(2021, 1, 1)
    end = date.today()
-   df = web.DataReader('AAPL', 'stooq', start, end)
+   df = web.get_data_yahoo('AAPL', start, end)
    df_EMAS = talib.EMA(df['Close'], timeperiod = 12)
    df_EMAL = talib.EMA(df['Close'], timeperiod = 26)
    df_EMAS.to_numpy()  
@@ -58,7 +60,7 @@ class RSI:
   def run(self):  
    start = dt.datetime(2021, 1, 1)
    end = date.today()
-   df = web.DataReader('TSLA', 'stooq', start, end)
+   df = web.get_data_yahoo('TSLA', start, end)
    df_RSI = talib.RSI(df['Close'])
    df_RSI.to_numpy()  
   
@@ -100,7 +102,7 @@ class BB:
   def run(self):  
    start = dt.datetime(2021, 1, 1)
    end = date.today()
-   df = web.DataReader('BA', 'stooq', start, end)
+   df = web.get_data_yahoo('BA', start, end)
    upper, mid, lower = talib.BBANDS(df['Close'], nbdevup=2, nbdevdn=2, timeperiod = 20)
    todayPrice = df['Close']
    todayPrice.to_numpy()
@@ -120,13 +122,50 @@ class BB:
    elif [((todayPrice.values[0]) < (upper.values[19])) & ((todayPrice.values[0]) > (lower.values[19]))]: 
     print("Stock is within mean value, holding position") #if price today is between upper and lower bands stock is within mean value    
        
+
+class DOJI:
+  def run(self):  
+   start = dt.datetime(2021, 1, 1)
+   end = date.today()
+   df = web.get_data_yahoo('DELL', start, end)
+   df_d = talib.CDLDOJISTAR(df['Open'], df['High'], df['Low'], df['Close'])
+   df_e = talib.CDLENGULFING(df['Open'], df['High'], df['Low'], df['Close'])
+   df_d.to_numpy()
+   df_e.to_numpy()
    
+   if (df_d.values[0] > 0):
+    print('The patterns indicates a trough postion on the Candlestick Graph, buying postion') 
+    api.submit_order(symbol='DELL', qty=100, side='buy')
+    
+   elif (df_e.values[0] > 0):
+    print('The patterns indicates a trough postion on the Candlestick Graph, buying postion') 
+    api.submit_order(symbol='DELL', qty=100, side='buy') 
+   
+   elif (df_d.values[0] < 0):
+    print('The patterns indicates a peak postion on the Candlestick Graph, selling postion') 
+    api.submit_order(symbol='DELL', qty=100, side='sell')  
+    
+   elif (df_e.values[0] < 0):
+    print('The patterns indicates a peak postion on the Candlestick Graph, selling postion') 
+    api.submit_order(symbol='DELL', qty=100, side='sell')  
+    
+   elif [(df_d.values[0] == 0) & (df_e.values[0] == 0)]:
+    print('The patterns indicates holding postion')     
+    
+   print('Searching historical values that do not equal zero so that we can see days where the conditions of Doji are met') 
+   print(df_d[df_d !=0])
+   print(df_e[df_e !=0])
+
+
+  
 rsi = RSI()
 adx = ADX()
 ema = EMA()
 bb = BB()
 di = DI()
+doji = DOJI()
 
+print("Running High Frequency Trading Strategies")
 print("Running EMA crossover strategy")
 ema.run()
 
@@ -141,3 +180,7 @@ di.run()
 
 print("Running Bollinger Bands mean revision strategy")
 bb.run() 
+
+print("Moving to Low Frequency Trading Strategies")
+print("Running Doji Star and Engulfing strategy")
+doji.run()
