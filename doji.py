22115c4cce19dda8426f585057474e8372c6cddd
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-09-02 11:12:29 +0100

    Update doji.py

--- doji.py
+++ doji.py
@@ -4,6 +4,11 @@ from datetime import date
 import pandas as pd 
 import pandas_datareader.data as web
 import talib
+import yfinance as yfin
+yfin.pdr_override()
+
+import pandas
+from pandas_datareader import data as pdr
 
 alpaca_endpoint = 'https://paper-api.alpaca.markets'
 api = tradeapi.REST('PKZMPG3T5B4KNAFUTRF3','JIvx2wgkPXtzGl9uy1ZSEryA5OBv9XZ37XPFwQGN', alpaca_endpoint)
@@ -13,9 +18,9 @@ class DOJI:
   def run(self):  
    start = dt.datetime(2021, 1, 1)
    end = date.today()
-   df = web.DataReader('SPY', 'google', start, end)
+   df = web.get_data_yahoo('SPY', start, end)
    df_d = talib.CDLDOJISTAR(df['Open'], df['High'], df['Low'], df['Close']) 
    print(df_d)
-    
+
 doji = DOJI()
 doji.run()
