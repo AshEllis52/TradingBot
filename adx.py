44df93543b381ce7635d0311787ec2493a2417bd
AshEllis52 <83059758+AshEllis52@users.noreply.github.com>
2021-09-04 08:23:34 +0100

    Update adx.py

--- adx.py
+++ adx.py
@@ -17,9 +17,9 @@ class ADX:
    adx = talib.ADX(df['High'], df['Low'], df['Close'], timeperiod = 21)
    adx.to_numpy()
   
-   print (adx[adx !='nan'])
-   print (adx.values[24])
-   print (adx.values[25])
+   print (adx.values[26])
+   print (adx.values[27])
+   print (adx.values[28])
    #if((adx.values[20]) <= 25):
     #print('The trend indicates little momentum, holding position') 
     
